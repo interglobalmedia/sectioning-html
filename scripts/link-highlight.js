73ce3890c5ebaaa5f4bff810f006a0a5075db983
Maria Campbell <interglobalmedia@gmail.com>
2021-04-15 11:31:57 -0400

    Add conditions for home page and submit page

--- scripts/link-highlight.js
+++ scripts/link-highlight.js
@@ -4,6 +4,12 @@ function navHighlight() {
 	    if (links[i].href === document.URL) {
 	        links[i].classList.add('active')
 	    }
+		if (links[0].href === document.URL || document.URL === 'https://www.letsbsocial.nyc/') {
+	        links[0].classList.add('active')
+	    }
+		if (links[4].href === document.URL || document.URL.includes('https://www.letsbsocial.nyc/submit.html')) {
+	        links[4].classList.add('active')
+	    }
 	}
 }
 
