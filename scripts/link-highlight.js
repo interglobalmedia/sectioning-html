44395adb54ae0b06f4797c990c1f98e544124df1
Maria Campbell <interglobalmedia@gmail.com>
2021-04-15 11:38:12 -0400

    Remove li

--- scripts/link-highlight.js
+++ scripts/link-highlight.js
@@ -1,5 +1,5 @@
 function navHighlight() {
-	const links = document.querySelectorAll('li .menu-link')
+	const links = document.querySelectorAll('.menu-link')
 	for (var i = 0; i < links.length; i++) {
 	    if (links[i].href === document.URL) {
 	        links[i].classList.add('active')
